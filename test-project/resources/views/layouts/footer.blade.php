c94c0e7d6df52eed1e1f64990b5db71480b1c082
Ela-El-maker <feloela101@gmail.com>
2024-05-27 11:34:58 +0300

    Working with views architecture

---
+++ test-project/resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="container" >
+    <div class="container bg-dark text-center">
+        <!-- Example row of columns -->
+        <hr>
+    </div>
+    <p>&copy;El's Company 2017-2018</p>
+</footer>
